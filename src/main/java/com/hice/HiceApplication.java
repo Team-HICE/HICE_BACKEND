f4d08f47b818bb618eab41fd51b3061627252679
GSB0203 <gimseungbin0203@gmail.com>
2024-12-10 19:52:35 +0900

    feat: git 기본설정

---
+++ src/main/java/com/hice/HiceApplication.java
@@ -0,0 +1,13 @@
+package com.hice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hice/HiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
